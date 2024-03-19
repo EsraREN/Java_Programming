41d4e5f864ffef73b31f35789be47d44d6e0be3b
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2024-03-19 14:25:30 +0300

    Update Kart.java

--- Hafta7/Ornek3/src/pkt/Kart.java
+++ Hafta7/Ornek3/src/pkt/Kart.java
@@ -1,15 +1,26 @@
 package pkt;
 
-
+/**
+ * Banka kartını temsil eden sınıf
+ * @author MFA
+ *
+ */
 public class Kart implements IKart {
 	private Hesap hesap;
 	private String sifre;
 	
 	public Kart(Hesap hesap, String sifre) {
+		/*
+		 * 
+		 */
 		this.hesap = hesap;
 		this.sifre = sifre;
 	}
 
+	/**
+	 * @param sifre Kart şifresi
+	 * @return Giriş başarılı mı?
+	 */
 	@Override
 	public boolean girisKontrol(String sifre) {
 		return this.sifre == sifre;
